cdfaec1c84dfa0a45b19dfd0558094b51d57c604
Eugene ONeill <oneill.eugene@gmail.com>
2013-11-05 18:51:23 -0800

    adding doc blocks to locale methods

--- lib/archetype/sass_extensions/functions/locale.rb
+++ lib/archetype/sass_extensions/functions/locale.rb
@@ -42,7 +42,10 @@ module Archetype::SassExtensions::Locale
   #
   # returns the locale language code
   #
-  # TODO
+  # *Parameters*:
+  # - <tt>$locale</tt> {String} the locale to examine
+  # *Returns*:
+  # - {String|Null} the language code within the locale string
   #
   def locale_language(locale = nil)
     return get_locale_piece(locale, :language)
@@ -51,7 +54,10 @@ module Archetype::SassExtensions::Locale
   #
   # returns the locale territory code
   #
-  # TODO
+  # *Parameters*:
+  # - <tt>$locale</tt> {String} the locale to examine
+  # *Returns*:
+  # - {String|Null} the territory code within the locale string
   #
   def locale_territory(locale = nil)
     return get_locale_piece(locale, :territory)
@@ -60,7 +66,10 @@ module Archetype::SassExtensions::Locale
   #
   # returns the locale modifier
   #
-  # TODO
+  # *Parameters*:
+  # - <tt>$locale</tt> {String} the locale to examine
+  # *Returns*:
+  # - {String|Null} the modifier code within the locale string
   #
   def locale_modifier(locale = nil)
     return get_locale_piece(locale, :modifier)
@@ -86,7 +95,14 @@ private
   #  (4) modifier (e.g. @Cyrillic)
   LOCALE_PATTERN = /\"?([a-z]{2})?[-_]?([a-z]{2})?(\.[^@]*)?(?:\@([^\"]+))?\"?/i
 
-  # TODO
+  #
+  # normalizes the locale string into an object
+  #
+  # *Parameters*:
+  # - <tt>locale</tt> {String} the locale to normalize
+  # *Returns*:
+  # - {Hash} the normalized locale object
+  #
   def normalize_locale(locale = nil)
     match = get_locale(locale).match(LOCALE_PATTERN) || []
     return {
@@ -97,7 +113,15 @@ private
     }
   end
 
-  # TODO
+  #
+  # get the locale from the given input
+  # if nil, will use either the current global locale in $CONFIG_LOCALE or the locale set on the compiler
+  #
+  # *Parameters*:
+  # - <tt>locale</tt> {Sass::String} the locale to use
+  # *Returns*:
+  # - {String} the locale string
+  #
   def get_locale(locale = nil)
     return (locale || environment.var('CONFIG_LOCALE') || Compass.configuration.locale || 'en_US').to_s
   end
