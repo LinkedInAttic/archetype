64586046e802b9a4b2255ed98b92f6256c1023c8
Eugene ONeill <oneill.eugene@gmail.com>
2013-11-17 07:01:13 -0800

    adding optimization to only output augmented shorthand if it was modified

--- lib/archetype/functions/css.rb
+++ lib/archetype/functions/css.rb
@@ -427,15 +427,16 @@ private
 
   def self.with_each_relative_if_root(related, property)
     styles = {}
+    augmented = false
     get_available_relatives(related, property).each do |key, value|
       # if it's the shorthand property...
-      if is_root_property?(key)
+      styles[normalize_property_key(key)] = value
+      augmented = !is_root_property?(key)
+      if !augmented
         styles = yield(value.to_a.clone, (value.is_a?(Sass::Script::Value::List) && value.separator == :comma)) if block_given?
-      else
-        styles[normalize_property_key(key)] = value
       end
     end
-    return styles
+    return styles, augmented
   end
 
   ####
@@ -489,7 +490,7 @@ private
   def self.handle_derived_properties_for_margin_padding(related, property)
     # we only care about the last piece of the property (e.g. `margin` or `top`)
     is_shorthand = is_root_property?(property)
-    styles = with_each_relative_if_root(related, property) do |items, comma_separated|
+    styles, augmented = with_each_relative_if_root(related, property) do |items, comma_separated|
       # blow away anything we've already discovered (because it's irrelevant)
       # and extract the top/right/bottom/left values
       # make the styles available to the calling context
@@ -501,7 +502,7 @@ private
       }
     end
     # if we're getting the shorthand property, reconstruct the shorthand value
-    if is_shorthand
+    if is_shorthand and augmented
       value = extrapolate_shorthand_margin_padding(styles)
       # if the value came back nil, we were missing something, so throw a warning...
       warn_not_enough_infomation_to_derive(property) if value.nil?
@@ -525,7 +526,7 @@ private
   def self.handle_derived_properties_for_animation(related, property)
     properties = %w(name duration timing-function delay iteration-count direction play-state)
     is_shorthand = is_root_property?(property)
-    styles = with_each_relative_if_root(related, property) do |items, comma_separated|
+    styles, augmented = with_each_relative_if_root(related, property) do |items, comma_separated|
       # blow away anything we've already discovered (because it's irrelevant)
       # identify the items that are timing units
       timings = get_timing_values(items)
@@ -560,7 +561,7 @@ private
       styles
     end
 
-    if is_shorthand
+    if is_shorthand and augmented
       value = extrapolate_shorthand_animation(styles)
       # if the value came back nil, we were missing something, so throw a warning...
       warn_not_enough_infomation_to_derive(property) if value.nil?
@@ -578,7 +579,7 @@ private
     properties = %w(color position size repeat origin clip attachment image)
     property_order = [:color, :position, :size, :repeat, :origin, :clip, :attachment, :image]
     is_shorthand = is_root_property?(property)
-    styles = with_each_relative_if_root(related, property) do |items, comma_separated|
+    styles, augmented = with_each_relative_if_root(related, property) do |items, comma_separated|
       i = 0
       # blow away anything we've already discovered (because it's irrelevant)
       styles = {}
@@ -644,7 +645,7 @@ private
       styles
     end
 
-    if is_shorthand
+    if is_shorthand and augmented
       if styles.nil?
         warn_not_enough_infomation_to_derive(property)
         return nil
@@ -710,7 +711,7 @@ private
     properties = %w(name new position)
     is_shorthand = is_root_property?(property)
     relatives = get_available_relatives(related, property)
-    styles = with_each_relative_if_root(related, property) do |items, comma_separated|
+    styles, augmented = with_each_relative_if_root(related, property) do |items, comma_separated|
       # blow away anything we've already discovered (because it's irrelevant)
       # target-name target-new target-position
       styles = { :name => items.shift }
@@ -728,7 +729,7 @@ private
       # make the styles available to the calling context
       styles
     end
-    if is_shorthand
+    if is_shorthand and augmented
       if styles.nil? or styles[:name].nil?
         warn_not_enough_infomation_to_derive(property)
         return nil
@@ -747,7 +748,7 @@ private
   def self.handle_derived_properties_for_transition(related, property)
     is_shorthand = is_root_property?(property)
     properties = %w(property duration timing-function delay)
-    styles = with_each_relative_if_root(related, property) do |items, comma_separated|
+    styles, augmented = with_each_relative_if_root(related, property) do |items, comma_separated|
       # blow away anything we've already discovered (because it's irrelevant)
       timings = get_timing_values(items)
       items = items - timings
@@ -773,7 +774,7 @@ private
       styles
     end
 
-    if is_shorthand
+    if is_shorthand and augmented
       if styles.nil? or styles[:property].nil?
         warn_not_enough_infomation_to_derive(property) if not styles.empty?
         return nil
